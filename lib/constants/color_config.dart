f73fd8cc7efbc940a11398fae026f8485dfddfef
Aliazimoshan <a.azimoshan@gmail.com>
2022-12-28 13:59:34 +0330

    create fontConfig file and add background & primarySwatch

---
+++ lib/constants/color_config.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ColorConfig {
+  static Color background = const Color(0xffE6E6E6);
+  static MaterialColor primarySwatch = const MaterialColor(
+    0xFF000000,
+    <int, Color>{
+      50: Color(0xFF000000),
+      100: Color(0xFF000000),
+      200: Color(0xFF000000),
+      300: Color(0xFF000000),
+      400: Color(0xFF000000),
+      500: Color(0xFF000000),
+      600: Color(0xFF000000),
+      700: Color(0xFF000000),
+      800: Color(0xFF000000),
+      900: Color(0xFF000000),
+    },
+  );
+}
