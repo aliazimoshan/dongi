af39124097a587e04b1e219f8ecf3e9a1dfd11cf
Aliazimoshan <a.azimoshan@gmail.com>
2022-12-28 13:59:49 +0330

    change fontConfigs

--- lib/constants/font_config.dart
+++ lib/constants/font_config.dart
@@ -3,15 +3,43 @@ import 'package:flutter/material.dart';
 class FontConfig {
   static TextStyle h1() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w600,
-        fontSize: 30,
+        fontWeight: FontWeight.w700,
+        fontSize: 24,
         color: Colors.black,
       );
 
-  static TextStyle p() => const TextStyle(
+  static TextStyle h2() => const TextStyle(
+        fontFamily: "Mulish",
+        fontWeight: FontWeight.w600,
+        fontSize: 20,
+        color: Colors.black,
+      );
+
+  static TextStyle h3() => const TextStyle(
+        fontFamily: "Mulish",
+        fontWeight: FontWeight.w600,
+        fontSize: 16,
+        color: Colors.black,
+      );
+
+  static TextStyle p1({Color? color}) => TextStyle(
+        fontFamily: "Mulish",
+        fontWeight: FontWeight.w400,
+        fontSize: 14,
+        color: color ?? Colors.black,
+      );
+
+  static TextStyle p2() => const TextStyle(
         fontFamily: "Mulish",
         fontWeight: FontWeight.w300,
-        fontSize: 17,
+        fontSize: 16,
+        color: Colors.black,
+      );
+
+  static TextStyle p3() => const TextStyle(
+        fontFamily: "Mulish",
+        fontWeight: FontWeight.w400,
+        fontSize: 10,
         color: Colors.black,
       );
 }
