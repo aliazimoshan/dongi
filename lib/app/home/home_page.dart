ac52311c038d9284f02a4e6879e14b42dddde53a
Aliazimoshan <a.azimoshan@gmail.com>
2022-12-29 20:41:34 +0330

    move appBar widget to widgets folder

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dongi/widgets/appbar/appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../constants/color_config.dart';
@@ -9,44 +10,7 @@ class HomePage extends ConsumerWidget with HomeWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(70),
-        child: Container(
-          color: ColorConfig.primarySwatch,
-          child: Column(
-            children: [
-              Expanded(
-                child: AppBar(
-                  elevation: 0,
-                  centerTitle: false,
-                  title: const Text("Dongi"),
-                  actions: [
-                    IconButton(
-                      icon: Container(
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.grey,
-                        ),
-                      ),
-                      onPressed: () {},
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                height: 15,
-                decoration: BoxDecoration(
-                  color: ColorConfig.background,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(15),
-                    topRight: Radius.circular(15),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+      appBar: AppBarWidget(),
       body: Column(
         children: [
           expenseSummery(),
