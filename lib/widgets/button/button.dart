533a6cf988dc9455f0bf615ff2d4de7001eede98
Aliazimoshan <a.azimoshan@gmail.com>
2022-12-29 20:39:53 +0330

    change structure of custom widget (button)

--- lib/widgets/button/button.dart
+++ lib/widgets/button/button.dart
@@ -2,47 +2,54 @@ import 'package:flutter/material.dart';
 import '../../constants/font_config.dart';
 
 @immutable
-class ButtonWidget extends StatelessWidget {
-  final String title;
-  final Function()? onPressed;
+class ButtonWidget extends ElevatedButton {
   final Color? backgroundColor;
   final Color? borderColor;
   final Color? textColor;
-
-  const ButtonWidget({
+  final String? title;
+  ButtonWidget({
     super.key,
-    required this.title,
-    required this.onPressed,
+    required super.onPressed,
+    this.title,
     this.backgroundColor,
     this.borderColor,
     this.textColor,
-  });
+  }) : super(child: child(textColor: textColor, title: title));
 
   @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: backgroundColor ?? Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15),
-            side: BorderSide(color: borderColor ?? Colors.black),
-          ),
-          elevation: 0,
-          padding: EdgeInsets.zero,
-          //padding: const EdgeInsets.symmetric(
-          //  horizontal: 15,
-          //  vertical: 15,
-          //),
-          //textStyle: const TextStyle(fontSize: 15),
+  ButtonStyle? get style => ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor ?? Colors.black,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+          side: BorderSide(color: borderColor ?? Colors.black),
         ),
-        child: Text(
-          title,
-          style: FontConfig.h6(color: textColor),
-        ),
+        elevation: 0,
+        padding: EdgeInsets.zero,
+        //padding: const EdgeInsets.symmetric(
+        //  horizontal: 15,
+        //  vertical: 15,
+        //),
+        //textStyle: const TextStyle(fontSize: 15),
+      );
+
+  ButtonWidget.outline({
+    this.backgroundColor = Colors.white,
+    this.borderColor = Colors.black,
+    this.textColor = Colors.black,
+    this.title,
+    super.key,
+    super.onPressed,
+  }) : super(child: child(textColor: textColor, title: title));
+}
+
+Widget child({String? title, Color? textColor}) {
+  return SizedBox(
+    height: 50,
+    child: Center(
+      child: Text(
+        title ?? "",
+        style: FontConfig.button(),
       ),
-    );
-  }
+    ),
+  );
 }
