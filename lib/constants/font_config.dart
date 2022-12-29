6824524412946e527bd33ce4b0b3218df959066f
Aliazimoshan <a.azimoshan@gmail.com>
2022-12-29 21:14:08 +0330

    remove color inputs for fonts

--- lib/constants/font_config.dart
+++ lib/constants/font_config.dart
@@ -1,38 +1,38 @@
 import 'package:flutter/material.dart';
 
 class FontConfig {
-  static TextStyle h4({Color? color}) => const TextStyle(
+  static TextStyle h4() => const TextStyle(
         fontFamily: "Mulish",
         fontWeight: FontWeight.w200,
         fontSize: 34,
         color: Colors.black,
       );
-  static TextStyle h6({Color? color}) => const TextStyle(
+  static TextStyle h6() => const TextStyle(
         fontFamily: "Mulish",
         fontWeight: FontWeight.w200,
         fontSize: 20,
         color: Colors.black,
       );
 
-  static TextStyle body1({Color? color}) => TextStyle(
+  static TextStyle body1() => const TextStyle(
         fontFamily: "Mulish",
         fontWeight: FontWeight.w200,
         fontSize: 16,
-        color: color ?? Colors.black,
+        color: Colors.black,
       );
 
-  static TextStyle body2({Color? color}) => TextStyle(
+  static TextStyle body2() => const TextStyle(
         fontFamily: "Mulish",
         fontWeight: FontWeight.w200,
         fontSize: 14,
-        color: color ?? Colors.black,
+        color: Colors.black,
       );
 
-  static TextStyle button({Color? color}) => TextStyle(
+  static TextStyle button() => const TextStyle(
         fontFamily: "Mulish",
         fontWeight: FontWeight.w300,
         fontSize: 14,
-        color: color ?? Colors.black,
+        color: Colors.white,
       );
 
   static TextStyle caption() => const TextStyle(
