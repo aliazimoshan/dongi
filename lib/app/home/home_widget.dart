00e0879af66040f73d1ebc7dc60777953c375402
Aliazimoshan <a.azimoshan@gmail.com>
2023-01-03 16:27:35 +0330

    remove expanded and add sized instead

--- lib/app/home/home_widget.dart
+++ lib/app/home/home_widget.dart
@@ -4,26 +4,23 @@ import '../../widgets/card/card.dart';
 
 class HomeWidget {
   Widget expenseSummery() {
-    return Expanded(
-      flex: 2,
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-        child: Row(
-          children: [
-            Expanded(child: _totalExpenseCard()),
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: Column(
-                  children: [
-                    _incomeCard(),
-                    _outcomeCard(),
-                  ],
-                ),
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+      child: Row(
+        children: [
+          Expanded(child: _totalExpenseCard()),
+          Expanded(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Column(
+                children: [
+                  _incomeCard(),
+                  _outcomeCard(),
+                ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -101,14 +98,11 @@ class HomeWidget {
   }
 
   recentGroup() {
-    return Expanded(
-      flex: 2,
-      child: Column(
-        children: [
-          _groupTitle(),
-          _groupCardLists(),
-        ],
-      ),
+    return Column(
+      children: [
+        _groupTitle(),
+        _groupCardLists(),
+      ],
     );
   }
 
@@ -118,7 +112,7 @@ class HomeWidget {
       child: Row(
         children: [
           Text(
-            "Group",
+            "Groups",
             style: FontConfig.h6(),
           ),
         ],
@@ -127,38 +121,151 @@ class HomeWidget {
   }
 
   _groupCardLists() {
-    return Expanded(
+    return SizedBox(
+      height: 150,
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: [
+          const SizedBox(width: 16),
           _groupCard(),
           _groupCard(),
           _groupCard(),
+          _moreCircle(),
         ],
       ),
     );
   }
 
   _groupCard() {
-    return CardWidget(
-        child: AspectRatio(
-      aspectRatio: 1,
-      child: Column(
+    boxCount() {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
+          Text(
+            "Boxes",
+            style: FontConfig.overline().copyWith(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          Text(
+            "5",
+            style: FontConfig.body2(),
+          ),
+        ],
+      );
+    }
+
+    memberRow() {
+      circleBox(Color color, {IconData? icon}) {
+        return Container(
+          width: 24,
+          height: 24,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: color,
+          ),
+          child: Icon(
+            icon,
+            color: Colors.white,
+            size: 12,
+          ),
+        );
+      }
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "members",
+            style: FontConfig.overline().copyWith(
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const SizedBox(height: 5),
           Row(
             children: [
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
+              Expanded(
+                child: Stack(
+                  children: [
+                    Positioned(
+                      left: 40,
+                      child: circleBox(Colors.grey.shade900),
+                    ),
+                    Positioned(
+                      left: 30,
+                      child: circleBox(Colors.grey.shade800),
+                    ),
+                    Positioned(
+                      left: 20,
+                      child: circleBox(Colors.grey.shade700),
+                    ),
+                    Positioned(
+                      left: 10,
+                      child: circleBox(Colors.grey.shade600),
+                    ),
+                    circleBox(Colors.grey.shade500),
+                  ],
                 ),
-                width: 32,
-                height: 32,
               ),
-              Text("Group Name")
+              circleBox(Colors.black, icon: Icons.share)
             ],
           )
         ],
+      );
+    }
+
+    return CardWidget(
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //group name widget
+            Row(
+              children: [
+                Container(
+                  width: 32,
+                  height: 32,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade400,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 5),
+                Text(
+                  "Group Name",
+                  style: FontConfig.overline().copyWith(
+                    fontWeight: FontWeight.w400,
+                  ),
+                )
+              ],
+            ),
+            //box count
+            boxCount(),
+            //member row
+            memberRow(),
+          ],
+        ),
       ),
-    ));
+    );
+  }
+
+  _moreCircle() {
+    return Container(
+      width: 48,
+      height: 48,
+      margin: const EdgeInsets.fromLTRB(10, 0, 16, 0),
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        shape: BoxShape.circle,
+      ),
+      child: const Icon(
+        Icons.arrow_forward,
+        color: Colors.white,
+      ),
+    );
   }
 }
