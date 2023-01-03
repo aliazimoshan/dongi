8a0a61c30947d908533acc6ca14ddd22ffc6b717
Aliazimoshan <a.azimoshan@gmail.com>
2023-01-03 15:11:52 +0330

    add group widget

--- lib/app/home/home_widget.dart
+++ lib/app/home/home_widget.dart
@@ -4,23 +4,26 @@ import '../../widgets/card/card.dart';
 
 class HomeWidget {
   Widget expenseSummery() {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
-      child: Row(
-        children: [
-          Expanded(child: _totalExpenseCard()),
-          Expanded(
-            child: AspectRatio(
-              aspectRatio: 1,
-              child: Column(
-                children: [
-                  _incomeCard(),
-                  _outcomeCard(),
-                ],
+    return Expanded(
+      flex: 2,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
+        child: Row(
+          children: [
+            Expanded(child: _totalExpenseCard()),
+            Expanded(
+              child: AspectRatio(
+                aspectRatio: 1,
+                child: Column(
+                  children: [
+                    _incomeCard(),
+                    _outcomeCard(),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -95,5 +98,67 @@ class HomeWidget {
         ),
       ),
     );
+  }
+
+  recentGroup() {
+    return Expanded(
+      flex: 2,
+      child: Column(
+        children: [
+          _groupTitle(),
+          _groupCardLists(),
+        ],
+      ),
+    );
+  }
+
+  _groupTitle() {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(26, 0, 10, 10),
+      child: Row(
+        children: [
+          Text(
+            "Group",
+            style: FontConfig.h6(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _groupCardLists() {
+    return Expanded(
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: [
+          _groupCard(),
+          _groupCard(),
+          _groupCard(),
+        ],
+      ),
+    );
+  }
+
+  _groupCard() {
+    return CardWidget(
+        child: AspectRatio(
+      aspectRatio: 1,
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                width: 32,
+                height: 32,
+              ),
+              Text("Group Name")
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }
