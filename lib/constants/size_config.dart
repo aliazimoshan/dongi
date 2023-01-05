122d813ae2a2d68a3f559cbf75aae2d26ebc2827
Aliazimoshan <a.azimoshan@gmail.com>
2023-01-05 19:41:55 +0330

    change confige with nullable

--- lib/constants/size_config.dart
+++ lib/constants/size_config.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class SizeConfig {
-  static double? width(context) => MediaQuery.of(context).size.width;
-  static double? height(context) => MediaQuery.of(context).size.height;
+  static double width(context) => MediaQuery.of(context).size.width;
+  static double height(context) => MediaQuery.of(context).size.height;
 }
