8b1d4769b916e30bd179168eed956864a6825302
Aliazimoshan <a.azimoshan@gmail.com>
2023-01-06 19:18:14 +0330

    add groupCard to groupWidget

--- lib/app/group/group_widget.dart
+++ lib/app/group/group_widget.dart
@@ -1,1 +1,22 @@
-class GroupWidget {}
+import 'package:flutter/material.dart';
+import '../../widgets/list_tile/custom_list_tile.dart';
+
+class GroupWidget {
+  groupCard(String title, String subtitle) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: CustomListTile(
+        title: title,
+        subTitle: subtitle,
+        icon: Container(
+          width: 50,
+          height: 50,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
