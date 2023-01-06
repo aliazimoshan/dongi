8b6a7ab202e3f94f6b426d74444b460a30080da5
Aliazimoshan <a.azimoshan@gmail.com>
2023-01-06 23:50:24 +0330

    finish group page and widget

---
+++ lib/app/group/create_group/create_group_page.dart
@@ -0,0 +1,26 @@
+import 'package:dongi/widgets/appbar/appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../constants/color_config.dart';
+import 'create_group_widget.dart';
+
+class CreateGroupPage extends ConsumerWidget with CreateGroupWidget {
+  const CreateGroupPage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      backgroundColor: ColorConfig.background,
+      appBar: AppBarWidget(title: "Create Group"),
+      body: Column(
+        children: [
+          groupInfoCard(context),
+          addFriendsCard(context),
+          const Spacer(),
+          createButton(),
+        ],
+      ),
+    );
+  }
+}
