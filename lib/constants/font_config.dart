d9dc3ba416677217adc8b41534f87a84f4ae5a5f
lhamsadeqi <elhamsadeqi.es98@gmail.com>
2023-01-08 20:36:31 +0330

    fixing font config, font weights

--- lib/constants/font_config.dart
+++ lib/constants/font_config.dart
@@ -3,56 +3,56 @@ import 'package:flutter/material.dart';
 class FontConfig {
   static TextStyle h4() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w200,
+        fontWeight: FontWeight.w400,
         fontSize: 34,
         color: Colors.black,
       );
 
   static TextStyle h5() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w200,
+        fontWeight: FontWeight.w400,
         fontSize: 25,
         color: Colors.black,
       );
 
   static TextStyle h6() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w200,
+        fontWeight: FontWeight.w500,
         fontSize: 20,
         color: Colors.black,
       );
 
   static TextStyle body1() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w200,
+        fontWeight: FontWeight.w400,
         fontSize: 16,
         color: Colors.black,
       );
 
   static TextStyle body2() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w200,
+        fontWeight: FontWeight.w400,
         fontSize: 14,
         color: Colors.black,
       );
 
   static TextStyle button() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.bold,
+        fontWeight: FontWeight.w500,
         fontSize: 14,
         color: Colors.white,
       );
 
   static TextStyle caption() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w100,
+        fontWeight: FontWeight.w400,
         fontSize: 12,
         color: Colors.black,
       );
 
   static TextStyle overline() => const TextStyle(
         fontFamily: "Mulish",
-        fontWeight: FontWeight.w100,
+        fontWeight: FontWeight.w400,
         fontSize: 10,
         color: Colors.black,
       );
