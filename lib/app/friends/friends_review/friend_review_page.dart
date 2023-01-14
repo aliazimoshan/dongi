e124d539a8dba590b9d64a45a6a1c0b053089e03
lhamsadeqi <elhamsadeqi.es98@gmail.com>
2023-01-14 19:17:35 +0330

    complete UI friends list page

--- lib/app/friends/friends_review/friend_review_page.dart
+++ lib/app/friends/friends_review/friend_review_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../constants/color_config.dart';
-import '../../constants/font_config.dart';
-import '../../widgets/appbar/sliver_appbar.dart';
+import '../../../constants/color_config.dart';
+import '../../../constants/font_config.dart';
+import '../../../widgets/appbar/sliver_appbar.dart';
 import './friend_review_widget.dart';
 
 class FriendReviewPage extends HookConsumerWidget with FriendReviewWidget {
