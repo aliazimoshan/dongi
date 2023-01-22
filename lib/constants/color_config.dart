369b7ee25bb068261d0f8f5da78e62c48973970e
lhamsdq <elhamsadeqi.es98@gmail.com>
2023-01-22 13:16:16 +0330

    adding color palette to color config

--- lib/constants/color_config.dart
+++ lib/constants/color_config.dart
@@ -7,12 +7,18 @@ class ColorConfig {
   static Color pureWhite = const Color(0xffFFFFFF);
   static Color baseGrey = const Color(0xffE6E6E6);
   static Color darkGrey = const Color(0xffD9D9D9);
+  static Color midnight = const Color(0xff0C1A26);
+  static Color secondary = const Color(0xff48BF7B);
+  static Color error = const Color(0xffD94F04);
+  static Color grey = const Color(0xffF2F2F2);
+  static Color white = const Color(0xffFFFFFF);
 
   /// * ----- primary swatch
   static MaterialColor primarySwatch = const MaterialColor(
-    0xFF000000,
+    0xFF19354D,
     <int, Color>{
-      50: Color(0xFF000000),
+      25: Color(0xFFC5CCD2),
+      50: Color(0xFF8C9AA6),
       100: Color(0xFF000000),
       200: Color(0xFF000000),
       300: Color(0xFF000000),
