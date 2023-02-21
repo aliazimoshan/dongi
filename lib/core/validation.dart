ce6fea12bb5dce8f7a44cadf520e66e79e35e532
Aliazimoshan <a.azimoshan@gmail.com>
2023-02-21 14:45:40 +0330

    add form validation stateNotifier

---
+++ lib/core/validation.dart
@@ -0,0 +1,35 @@
+import 'package:dongi/extensions/validation_string.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class FormValidatorNotifier extends StateNotifier<String?> {
+  FormValidatorNotifier() : super(null);
+
+  String? validatePassword(String? value) {
+    if (value != null && value.isValidPassword) {
+      return null;
+    } else {
+      return 'Password must contain an uppercase, lowercase, numeric digit and special character';
+    }
+  }
+
+  String? validateEmail(String? value) {
+    if (value != null && value.isValidEmail) {
+      return null;
+    } else {
+      return 'Please Enter a Valid Email';
+    }
+  }
+
+  String? validateUsername(String? value) {
+    if (value != null && value.length >= 4) {
+      return null;
+    } else {
+      return 'Please Enter a Valid Email';
+    }
+  }
+}
+
+final formValidatorProvider =
+    StateNotifierProvider<FormValidatorNotifier, String?>((ref) {
+  return FormValidatorNotifier();
+});
