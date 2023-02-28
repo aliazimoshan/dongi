67c58d5d89122c5d776dc5d7a4df4105f1f2bc2e
Aliazimoshan <a.azimoshan@gmail.com>
2023-02-28 23:58:28 +0330

    add appwrite config

--- lib/constants/appwrite_config.dart
+++ lib/constants/appwrite_config.dart
@@ -3,4 +3,8 @@ class AppwriteConfig {
   static const String databaseId = "63d7f76893cd2c0af894";
   static const String endPoint = "https://api.dispose.ir/v1";
   static const String usersCollection = '63d7f7862d321a55caa8';
+  static const String groupCollection = '63fca819d4c90825a5ba';
+  static const String imagesBucket = '63fccbd9199c4c5f9142';
+  static String imageUrl(String imageId) =>
+      '$endPoint/storage/buckets/$imagesBucket/files/$imageId/view?project=$projectId&mode=admin';
 }
