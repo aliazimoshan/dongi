a7c699d7d72c659e7659245edde2694d8c2d5aa4
Aliazimoshan <a.azimoshan@gmail.com>
2023-03-01 23:44:50 +0330

    add action for test

--- lib/app/group/group_list/group_list_page.dart
+++ lib/app/group/group_list/group_list_page.dart
@@ -1,7 +1,9 @@
+import 'package:dongi/models/group_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../widgets/appbar/appbar.dart';
 import '../../../widgets/floating_action_button/floating_action_button.dart';
+import '../controller/group_controller.dart';
 import 'group_list_widget.dart';
 
 class GroupListPage extends ConsumerWidget with GroupListWidget {
@@ -11,7 +13,17 @@ class GroupListPage extends ConsumerWidget with GroupListWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBarWidget(title: "Groups"),
-      floatingActionButton:  FloatingActionButtonWidget(),
+      floatingActionButton: FloatingActionButtonWidget(
+        onPressed: () => ref.read(groupControllerProvider.notifier).addGroup(
+              context: context,
+              groupModel: GroupModel(
+                title: "title",
+                description: "description",
+                creatorId: "creatorId",
+                members: [],
+              ),
+            ),
+      ),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
         child: ListView(
