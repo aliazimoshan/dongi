adb76884aa8234b58d5e2bb67f209ffdca8d43d8
Aliazimoshan <a.azimoshan@gmail.com>
2023-03-20 23:28:27 +0330

    add new route

--- lib/constants/route_config.dart
+++ lib/constants/route_config.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../app/group/create_group/create_group_page.dart';
 import '../app/register/auth_controller/auth_controller.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
@@ -21,6 +22,7 @@ class RouteNameConfig {
   static String signup = '/signup';
   static String onboarding = '/onboarding';
   static String groupList = '/group';
+  static String createGroup = '/group/create';
 }
 
 GoRouter _goRouterConfig(StateProviderRef ref) {
@@ -48,13 +50,27 @@ GoRouter _goRouterConfig(StateProviderRef ref) {
         path: RouteNameConfig.groupList,
         builder: (context, state) => GroupListPage(),
       ),
+      GoRoute(
+        path: RouteNameConfig.createGroup,
+        builder: (context, state) => CreateGroupPage(),
+      ),
     ],
     redirect: (context, state) async {
+      bool isUserSigningIn = false;
+      if (state.location == RouteNameConfig.signup ||
+          state.location == RouteNameConfig.signin) {
+        //user try to sign in or sign up
+        isUserSigningIn = true;
+      }
+
+      if (isUserSigningIn) return null;
+
       final currentUserProvider =
           await ref.read(authControllerProvider.notifier).currentUser();
       if (currentUserProvider != null) {
         return null;
       }
+
       return RouteNameConfig.signin;
     },
   );
