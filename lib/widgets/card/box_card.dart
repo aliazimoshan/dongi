d0d0b21f82d5bffd9b156abbf687b55cc5517249
Aliazimoshan <a.azimoshan@gmail.com>
2023-04-11 12:09:42 +0330

    add dropdown to box card

--- lib/widgets/card/box_card.dart
+++ lib/widgets/card/box_card.dart
@@ -1,18 +1,62 @@
 import 'package:dongi/models/box_model.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../app/box/controller/box_controller.dart';
 import '../../constants/color_config.dart';
 import '../../constants/font_config.dart';
+import '../../constants/route_config.dart';
 import 'card.dart';
 
-class BoxCardWidget extends CardWidget {
+class BoxCardWidget extends StatelessWidget {
   final BoxModel box;
+
   const BoxCardWidget(this.box, {super.key});
 
-  @override
-  EdgeInsetsGeometry? get margin => const EdgeInsets.fromLTRB(5, 0, 5, 0);
+  ///* Drop down
+  _dropdownButton({
+    required BuildContext context,
+    required BoxModel boxModel,
+  }) {
+    List<String> items = ["Edit", "Delete"];
+
+    return Consumer(
+      builder: (context, ref, child) {
+        return DropdownButton<String>(
+          icon: const Icon(Icons.more_vert_outlined),
+          items: items.map((String item) {
+            return DropdownMenuItem(
+              value: item,
+              child: Text(item),
+            );
+          }).toList(),
+          onChanged: (val) {
+            if (val == items[0]) {
+              //Edit dropdown action
+              context.push(
+                RouteNameConfig.updateBox,
+                extra: boxModel,
+              );
+            } else {
+              //Delete dropdown action
+              ref
+                  .read(boxControllerProvider.notifier)
+                  .deleteBox(context: context, ref: ref, boxModel: boxModel);
+            }
+          },
+          underline: Container(),
+          isDense: true,
+        );
+      },
+    );
+  }
 
   @override
-  Widget? get child => Padding(
+  Widget build(BuildContext context) {
+    return CardWidget(
+      padding: EdgeInsets.zero,
+      margin: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+      child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: SizedBox(
           width: 150,
@@ -35,22 +79,21 @@ class BoxCardWidget extends CardWidget {
                   ),
                   const SizedBox(width: 5),
                   Expanded(child: Text(box.title, style: FontConfig.body2())),
-                  Icon(
-                    Icons.more_vert,
-                    color: ColorConfig.primarySwatch,
-                  ),
+                  _dropdownButton(boxModel: box, context: context)
                 ],
               ),
               const Spacer(),
               Text("Total bill", style: FontConfig.overline()),
+              const SizedBox(height: 2),
               Text(
-                '\$140.0',
+                '\$${box.total}',
                 style: FontConfig.body2().copyWith(
                   fontWeight: FontWeight.w600,
                 ),
               ),
               const Spacer(),
               Text("Split to", style: FontConfig.overline()),
+              const SizedBox(height: 2),
               Text(
                 box.members.length.toString(),
                 style: FontConfig.body2().copyWith(
@@ -61,5 +104,7 @@ class BoxCardWidget extends CardWidget {
             ],
           ),
         ),
-      );
+      ),
+    );
+  }
 }
