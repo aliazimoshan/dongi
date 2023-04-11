4462dcbea45cd198763a02ca8a567a22bd7d41f6
Payam Zahedi <pzahedi1995@gmail.com>
2023-04-11 20:39:44 +0330

    refactor the sign in UI

--- lib/app/register/sign_in/sign_in_page.dart
+++ lib/app/register/sign_in/sign_in_page.dart
@@ -1,3 +1,4 @@
+import 'package:dongi/app/register/auth_controller/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -5,7 +6,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../constants/color_config.dart';
 import './sign_in_widget.dart';
 
-class SignInPage extends HookConsumerWidget with SignInWidget {
+class SignInPage extends HookConsumerWidget {
   SignInPage({super.key});
   final _formKey = GlobalKey<FormState>();
 
@@ -21,24 +22,39 @@ class SignInPage extends HookConsumerWidget with SignInWidget {
           const Expanded(
             child: SizedBox(),
           ),
-          signinBody(
-            children: [
-              title(),
-              form(
-                ref: ref,
-                formKey: _formKey,
-                email: emailController,
-                password: passwordController,
+          Container(
+            padding: const EdgeInsets.fromLTRB(50, 30, 50, 50),
+            decoration: BoxDecoration(
+              color: ColorConfig.white,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(16),
+                topRight: Radius.circular(16),
               ),
-              actionButton(
-                formKey: _formKey,
-                context: context,
-                ref: ref,
-                email: emailController,
-                password: passwordController,
-              ),
-              changeActionButton(context),
-            ],
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SignInTitle(),
+                SignInForm(
+                  formKey: _formKey,
+                  email: emailController,
+                  password: passwordController,
+                ),
+                SignInActionButton(
+                  isLoading: ref.watch(authControllerProvider),
+                  onTap: () {
+                    if (_formKey.currentState!.validate()) {
+                      ref.read(authControllerProvider.notifier).signIn(
+                            context: context,
+                            email: emailController.text,
+                            password: passwordController.text,
+                          );
+                    }
+                  },
+                ),
+                const SignInChangeActionButton(),
+              ],
+            ),
           ),
         ],
       ),
