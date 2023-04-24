6d2d96645c1513f8dd9102fe0bd8afc12f6d4d34
Aliazimoshan <a.azimoshan@gmail.com>
2023-04-24 22:30:05 +0330

    move global provider to provider directory

--- lib/providers/appwrite_provider.dart
+++ lib/providers/appwrite_provider.dart
@@ -29,7 +29,36 @@ final appwriteRealtimeProvider = Provider((ref) {
   final client = ref.watch(appwriteClientProvider);
   return Realtime(client);
 });
+
 final appwriteFunctionProvider = Provider((ref) {
   final client = ref.watch(appwriteClientProvider);
   return Functions(client);
 });
+
+
+
+
+
+//final appWriteInitializeProvider = FutureProvider.autoDispose((ref) async {
+//  try {
+//    //final projects = ref.watch(appWriteProjectsProvider);
+//    final database = ref.watch(appwriteDatabaseProvider);
+
+
+//    // Create a new collection
+//    final collection = await database.create(
+//      name: 'My Collection',
+//      read: ['*'],
+//      write: ['*'],
+//      rules: [],
+//      project:
+//          project['\$id'], // Use the project ID returned by the create method
+//    );
+
+//    return collection;
+//  } on AppwriteException catch (e) {
+//    // Handle any errors that occur
+//    print('Error creating collection or project: ${e.message}');
+//    rethrow;
+//  }
+//});
