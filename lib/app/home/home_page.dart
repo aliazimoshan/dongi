957409fe686c89558b8ce986df5355d3edd57295
Aliazimoshan <a.azimoshan@gmail.com>
2023-05-21 11:00:37 +0330

    dart fix result

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,12 +1,11 @@
-import 'package:dongi/constants/color_config.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../../constants/color_config.dart';
 import '../../widgets/appbar/appbar.dart';
-import '../auth/controller/auth_controller.dart';
 import 'home_widget.dart';
 
 class HomePage extends ConsumerWidget {
-  HomePage({super.key});
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
