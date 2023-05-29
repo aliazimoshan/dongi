627c3bcac9667d931f4eada15177a6b75fde27aa
Aliazimoshan <a.azimoshan@gmail.com>
2023-05-29 10:55:23 +0330

    change the structure of create box page

--- lib/app/box/create_box/create_box_page.dart
+++ lib/app/box/create_box/create_box_page.dart
@@ -1,13 +1,16 @@
 import 'dart:io';
 
+import 'package:dongi/app/box/controller/box_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../constants/color_config.dart';
+import '../../../core/utils.dart';
 import '../../../widgets/appbar/appbar.dart';
 import 'create_box_widget.dart';
 
-class CreateBoxPage extends HookConsumerWidget with CreateBoxWidget {
+class CreateBoxPage extends HookConsumerWidget {
   final String groupId;
   CreateBoxPage(this.groupId, {super.key});
   final _formKey = GlobalKey<FormState>();
@@ -18,24 +21,33 @@ class CreateBoxPage extends HookConsumerWidget with CreateBoxWidget {
     final boxDescription = useTextEditingController();
     final image = useState<File?>(null);
 
+    /// by using listen we are not gonna rebuild our app
+    ref.listen<BoxState>(
+      boxNotifierProvider,
+      (previous, next) {
+        next.whenOrNull(
+            loaded: () {
+              showSnackBar(context, "Successfully Created!!");
+              context.pop();
+            },
+            error: (message) => showSnackBar(context, message));
+      },
+    );
+
     return Scaffold(
       backgroundColor: ColorConfig.white,
       appBar: AppBarWidget(title: "Create Box"),
       body: Column(
         children: [
-          boxInfoCard(
-            context: context,
+          CreateBoxInfoCard(
             image: image,
             boxTitle: boxTitle,
             boxDescription: boxDescription,
             formKey: _formKey,
-            ref: ref,
           ),
-          selectFriendsCard(context),
+          const CreateBoxSelectFriends(),
           const Spacer(),
-          createButton(
-            ref: ref,
-            context: context,
+          CreateBoxButton(
             formKey: _formKey,
             image: image,
             boxTitle: boxTitle,
--- lib/app/box/create_box/create_box_widget.dart
+++ lib/app/box/create_box/create_box_widget.dart
@@ -14,53 +14,89 @@ import '../../../widgets/friends/friend.dart';
 import '../../../widgets/text_field/text_field.dart';
 import '../controller/box_controller.dart';
 
-class CreateBoxWidget {
-  /// * ----- box info card
-  boxInfoCard({
-    required BuildContext context,
-    required TextEditingController boxTitle,
-    required TextEditingController boxDescription,
-    required ValueNotifier<File?> image,
-    required GlobalKey<FormState> formKey,
-    required WidgetRef ref,
-  }) {
-    /// * ----- select option button
-    selectOptionButton({
-      required Function onTap,
-      required String icon,
-      required String title,
-    }) {
-      return InkWell(
-        onTap: () {
-          onTap();
-        },
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          height: 50,
-          decoration: BoxDecoration(
-            color: ColorConfig.white,
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Row(
-            children: [
-              SvgPicture.asset(icon),
-              const SizedBox(width: 10),
-              Text(
-                title,
-                style: FontConfig.body2(),
-              ),
-              const Spacer(),
-              Icon(
-                Icons.arrow_forward_ios_rounded,
-                color: ColorConfig.primarySwatch,
-                size: 20,
-              )
-            ],
-          ),
-        ),
-      );
-    }
+class CreateBoxInfoCard extends ConsumerWidget {
+  final TextEditingController boxTitle;
+  final TextEditingController boxDescription;
+  final ValueNotifier<File?> image;
+  final GlobalKey<FormState> formKey;
 
+  const CreateBoxInfoCard({
+    super.key,
+    required this.boxTitle,
+    required this.boxDescription,
+    required this.image,
+    required this.formKey,
+  });
+
+  selectOptionButton({
+    required Function onTap,
+    required String icon,
+    required String title,
+  }) {
+    return InkWell(
+      onTap: () {
+        onTap();
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        height: 50,
+        decoration: BoxDecoration(
+          color: ColorConfig.white,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            SvgPicture.asset(icon),
+            const SizedBox(width: 10),
+            Text(
+              title,
+              style: FontConfig.body2(),
+            ),
+            const Spacer(),
+            Icon(
+              Icons.arrow_forward_ios_rounded,
+              color: ColorConfig.primarySwatch,
+              size: 20,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  /// * ----- add photo button
+  _addPhotoButton(ValueNotifier<File?> image) {
+    return InkWell(
+      onTap: () async {
+        image.value = await pickImage();
+      },
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: BoxDecoration(
+          color: ColorConfig.white,
+          borderRadius: BorderRadius.circular(10),
+          image: image.value != null
+              ? DecorationImage(
+                  image: FileImage(image.value!),
+                  fit: BoxFit.cover,
+                )
+              : null,
+        ),
+        child: image.value == null
+            ? Center(
+                child: SvgPicture.asset(
+                  'assets/svg/add_photo_icon.svg',
+                  height: 14,
+                ),
+              )
+            : null,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Form(
       key: formKey,
       child: Container(
@@ -107,60 +143,74 @@ class CreateBoxWidget {
       ),
     );
   }
+}
 
-  /// * ----- select friends card
-  selectFriendsCard(BuildContext context) => Padding(
-        padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'select member',
-              style: FontConfig.body1(),
+class CreateBoxSelectFriends extends ConsumerWidget {
+  const CreateBoxSelectFriends({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'select member',
+            style: FontConfig.body1(),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
+            width: SizeConfig.width(context),
+            decoration: BoxDecoration(
+              color: ColorConfig.grey,
+              borderRadius: BorderRadius.circular(15),
             ),
-            const SizedBox(height: 10),
-            Container(
-              padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-              width: SizeConfig.width(context),
-              decoration: BoxDecoration(
-                color: ColorConfig.grey,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GridView.builder(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemCount: 12,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      // childAspectRatio: ,
-                    ),
-                    itemBuilder: (context, i) => FriendWidget(
-                      backgroundColor: ColorConfig.white,
-                    ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 12,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    // childAspectRatio: ,
                   ),
-                ],
-              ),
+                  itemBuilder: (context, i) => FriendWidget(
+                    backgroundColor: ColorConfig.white,
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      );
+          ),
+        ],
+      ),
+    );
+  }
+}
 
-  /// * ----- submit button
-  createButton({
-    required WidgetRef ref,
-    required BuildContext context,
-    required ValueNotifier<File?> image,
-    required TextEditingController boxTitle,
-    required TextEditingController boxDescription,
-    required GlobalKey<FormState> formKey,
-    required String groupId,
-  }) {
+class CreateBoxButton extends ConsumerWidget {
+  final ValueNotifier<File?> image;
+  final TextEditingController boxTitle;
+  final TextEditingController boxDescription;
+  final GlobalKey<FormState> formKey;
+  final String groupId;
+
+  const CreateBoxButton({
+    super.key,
+    required this.image,
+    required this.boxTitle,
+    required this.boxDescription,
+    required this.formKey,
+    required this.groupId,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(16, 0, 16, 30),
       child: ButtonWidget(
@@ -171,8 +221,6 @@ class CreateBoxWidget {
         onPressed: () {
           if (formKey.currentState!.validate()) {
             ref.read(boxNotifierProvider.notifier).addBox(
-                  ref: ref,
-                  context: context,
                   image: image,
                   boxTitle: boxTitle,
                   boxDescription: boxDescription,
@@ -182,37 +230,6 @@ class CreateBoxWidget {
         },
         title: 'create',
         textColor: ColorConfig.secondary,
-      ),
-    );
-  }
-
-  /// * ----- add photo button
-  _addPhotoButton(ValueNotifier<File?> image) {
-    return InkWell(
-      onTap: () async {
-        image.value = await pickImage();
-      },
-      child: Container(
-        width: 50,
-        height: 50,
-        decoration: BoxDecoration(
-          color: ColorConfig.white,
-          borderRadius: BorderRadius.circular(10),
-          image: image.value != null
-              ? DecorationImage(
-                  image: FileImage(image.value!),
-                  fit: BoxFit.cover,
-                )
-              : null,
-        ),
-        child: image.value == null
-            ? Center(
-                child: SvgPicture.asset(
-                  'assets/svg/add_photo_icon.svg',
-                  height: 14,
-                ),
-              )
-            : null,
       ),
     );
   }
