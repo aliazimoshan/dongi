ce5e3652deaf6509d7bd833a68bd1e6b981f1e05
Aliazimoshan <a.azimoshan@gmail.com>
2023-05-31 12:28:49 +0330

    add group controller

---
+++ lib/app/friends/controller/friend_controller.dart
@@ -0,0 +1,96 @@
+import 'package:dongi/models/user_friend_model.dart';
+import 'package:dongi/models/user_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../services/auth_service.dart';
+import '../../../services/friend_service.dart';
+import '../../auth/controller/auth_controller.dart';
+part 'friend_controller.freezed.dart';
+
+final friendNotifierProvider =
+    StateNotifierProvider<FriendNotifier, FriendState>(
+  (ref) {
+    return FriendNotifier(
+      ref: ref,
+      friendAPI: ref.watch(friendAPIProvider),
+    );
+  },
+);
+
+@freezed
+class FriendState with _$FriendState {
+  const factory FriendState.init() = FriendInitState;
+  const factory FriendState.loading() = FriendLoadingState;
+  const factory FriendState.loaded() = FriendLoadedState;
+  const factory FriendState.error(String message) = FriendErrorState;
+}
+
+final getFriendProvider = FutureProvider((ref) {
+  final friendController = ref.watch(friendNotifierProvider.notifier);
+  return friendController.getFriends();
+});
+
+final getFriendDetailProvider =
+    FutureProvider.family.autoDispose((ref, String friendId) {
+  final friendController = ref.watch(friendNotifierProvider.notifier);
+  return friendController.getFriendDetail(friendId);
+});
+
+class FriendNotifier extends StateNotifier<FriendState> {
+  FriendNotifier({
+    required this.ref,
+    required this.friendAPI,
+  }) : super(const FriendState.init());
+
+  final Ref ref;
+  final FriendAPI friendAPI;
+
+  Future<void> addFriend(UserModel userModel) async {
+    state = const FriendState.loading();
+    final currentUser = await ref.read(authAPIProvider).currentUserAccount();
+    final currentUserModel = await ref
+        .read(authControllerProvider.notifier)
+        .getUserData(currentUser!.$id);
+
+    UserFriendModel friendModel = UserFriendModel(
+      sendRequestUserId: currentUserModel.id!,
+      sendRequestUserName: currentUserModel.userName,
+      sendRequestProfilePic: currentUserModel.profilePic,
+      receiveRequestUserId: userModel.id!,
+      receiveRequestUserName: userModel.userName,
+      receiveRequestProfilePic: userModel.profilePic,
+    );
+
+    final res = await friendAPI.addFriend(friendModel);
+    state = res.fold(
+      (l) => FriendState.error(l.message),
+      (r) => const FriendState.loaded(),
+    );
+  }
+
+  Future<void> deleteFriend(UserFriendModel friendModel) async {
+    state = const FriendState.loading();
+    //remove friend from server
+    final res = await friendAPI.deleteFriend(friendModel.id!);
+
+    state = res.fold(
+      (l) => FriendState.error(l.message),
+      (r) => const FriendState.loaded(),
+    );
+  }
+
+  Future<List<UserFriendModel>> getFriends() async {
+    final user = await ref.read(authAPIProvider).currentUserAccount();
+    final friendList = await friendAPI.getFriends(user!.$id);
+    return friendList
+        .map((friend) => UserFriendModel.fromJson(friend.data))
+        .toList();
+  }
+
+  Future<UserFriendModel> getFriendDetail(String friendId) async {
+    final user = await ref.read(authAPIProvider).currentUserAccount();
+    final friend = await friendAPI.getFriendDetail(user!.$id, friendId);
+    return UserFriendModel.fromJson(friend.data);
+  }
+}
---
+++ lib/app/friends/controller/friend_controller.freezed.dart
@@ -0,0 +1,574 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'friend_controller.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$FriendState {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() init,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+    required TResult Function(String message) error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? init,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+    TResult? Function(String message)? error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? init,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    TResult Function(String message)? error,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(FriendInitState value) init,
+    required TResult Function(FriendLoadingState value) loading,
+    required TResult Function(FriendLoadedState value) loaded,
+    required TResult Function(FriendErrorState value) error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(FriendInitState value)? init,
+    TResult? Function(FriendLoadingState value)? loading,
+    TResult? Function(FriendLoadedState value)? loaded,
+    TResult? Function(FriendErrorState value)? error,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(FriendInitState value)? init,
+    TResult Function(FriendLoadingState value)? loading,
+    TResult Function(FriendLoadedState value)? loaded,
+    TResult Function(FriendErrorState value)? error,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $FriendStateCopyWith<$Res> {
+  factory $FriendStateCopyWith(
+          FriendState value, $Res Function(FriendState) then) =
+      _$FriendStateCopyWithImpl<$Res, FriendState>;
+}
+
+/// @nodoc
+class _$FriendStateCopyWithImpl<$Res, $Val extends FriendState>
+    implements $FriendStateCopyWith<$Res> {
+  _$FriendStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$FriendInitStateCopyWith<$Res> {
+  factory _$$FriendInitStateCopyWith(
+          _$FriendInitState value, $Res Function(_$FriendInitState) then) =
+      __$$FriendInitStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$FriendInitStateCopyWithImpl<$Res>
+    extends _$FriendStateCopyWithImpl<$Res, _$FriendInitState>
+    implements _$$FriendInitStateCopyWith<$Res> {
+  __$$FriendInitStateCopyWithImpl(
+      _$FriendInitState _value, $Res Function(_$FriendInitState) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$FriendInitState implements FriendInitState {
+  const _$FriendInitState();
+
+  @override
+  String toString() {
+    return 'FriendState.init()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$FriendInitState);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() init,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+    required TResult Function(String message) error,
+  }) {
+    return init();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? init,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+    TResult? Function(String message)? error,
+  }) {
+    return init?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? init,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    TResult Function(String message)? error,
+    required TResult orElse(),
+  }) {
+    if (init != null) {
+      return init();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(FriendInitState value) init,
+    required TResult Function(FriendLoadingState value) loading,
+    required TResult Function(FriendLoadedState value) loaded,
+    required TResult Function(FriendErrorState value) error,
+  }) {
+    return init(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(FriendInitState value)? init,
+    TResult? Function(FriendLoadingState value)? loading,
+    TResult? Function(FriendLoadedState value)? loaded,
+    TResult? Function(FriendErrorState value)? error,
+  }) {
+    return init?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(FriendInitState value)? init,
+    TResult Function(FriendLoadingState value)? loading,
+    TResult Function(FriendLoadedState value)? loaded,
+    TResult Function(FriendErrorState value)? error,
+    required TResult orElse(),
+  }) {
+    if (init != null) {
+      return init(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class FriendInitState implements FriendState {
+  const factory FriendInitState() = _$FriendInitState;
+}
+
+/// @nodoc
+abstract class _$$FriendLoadingStateCopyWith<$Res> {
+  factory _$$FriendLoadingStateCopyWith(_$FriendLoadingState value,
+          $Res Function(_$FriendLoadingState) then) =
+      __$$FriendLoadingStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$FriendLoadingStateCopyWithImpl<$Res>
+    extends _$FriendStateCopyWithImpl<$Res, _$FriendLoadingState>
+    implements _$$FriendLoadingStateCopyWith<$Res> {
+  __$$FriendLoadingStateCopyWithImpl(
+      _$FriendLoadingState _value, $Res Function(_$FriendLoadingState) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$FriendLoadingState implements FriendLoadingState {
+  const _$FriendLoadingState();
+
+  @override
+  String toString() {
+    return 'FriendState.loading()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$FriendLoadingState);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() init,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+    required TResult Function(String message) error,
+  }) {
+    return loading();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? init,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+    TResult? Function(String message)? error,
+  }) {
+    return loading?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? init,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    TResult Function(String message)? error,
+    required TResult orElse(),
+  }) {
+    if (loading != null) {
+      return loading();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(FriendInitState value) init,
+    required TResult Function(FriendLoadingState value) loading,
+    required TResult Function(FriendLoadedState value) loaded,
+    required TResult Function(FriendErrorState value) error,
+  }) {
+    return loading(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(FriendInitState value)? init,
+    TResult? Function(FriendLoadingState value)? loading,
+    TResult? Function(FriendLoadedState value)? loaded,
+    TResult? Function(FriendErrorState value)? error,
+  }) {
+    return loading?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(FriendInitState value)? init,
+    TResult Function(FriendLoadingState value)? loading,
+    TResult Function(FriendLoadedState value)? loaded,
+    TResult Function(FriendErrorState value)? error,
+    required TResult orElse(),
+  }) {
+    if (loading != null) {
+      return loading(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class FriendLoadingState implements FriendState {
+  const factory FriendLoadingState() = _$FriendLoadingState;
+}
+
+/// @nodoc
+abstract class _$$FriendLoadedStateCopyWith<$Res> {
+  factory _$$FriendLoadedStateCopyWith(
+          _$FriendLoadedState value, $Res Function(_$FriendLoadedState) then) =
+      __$$FriendLoadedStateCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$FriendLoadedStateCopyWithImpl<$Res>
+    extends _$FriendStateCopyWithImpl<$Res, _$FriendLoadedState>
+    implements _$$FriendLoadedStateCopyWith<$Res> {
+  __$$FriendLoadedStateCopyWithImpl(
+      _$FriendLoadedState _value, $Res Function(_$FriendLoadedState) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$FriendLoadedState implements FriendLoadedState {
+  const _$FriendLoadedState();
+
+  @override
+  String toString() {
+    return 'FriendState.loaded()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$FriendLoadedState);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() init,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+    required TResult Function(String message) error,
+  }) {
+    return loaded();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? init,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+    TResult? Function(String message)? error,
+  }) {
+    return loaded?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? init,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    TResult Function(String message)? error,
+    required TResult orElse(),
+  }) {
+    if (loaded != null) {
+      return loaded();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(FriendInitState value) init,
+    required TResult Function(FriendLoadingState value) loading,
+    required TResult Function(FriendLoadedState value) loaded,
+    required TResult Function(FriendErrorState value) error,
+  }) {
+    return loaded(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(FriendInitState value)? init,
+    TResult? Function(FriendLoadingState value)? loading,
+    TResult? Function(FriendLoadedState value)? loaded,
+    TResult? Function(FriendErrorState value)? error,
+  }) {
+    return loaded?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(FriendInitState value)? init,
+    TResult Function(FriendLoadingState value)? loading,
+    TResult Function(FriendLoadedState value)? loaded,
+    TResult Function(FriendErrorState value)? error,
+    required TResult orElse(),
+  }) {
+    if (loaded != null) {
+      return loaded(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class FriendLoadedState implements FriendState {
+  const factory FriendLoadedState() = _$FriendLoadedState;
+}
+
+/// @nodoc
+abstract class _$$FriendErrorStateCopyWith<$Res> {
+  factory _$$FriendErrorStateCopyWith(
+          _$FriendErrorState value, $Res Function(_$FriendErrorState) then) =
+      __$$FriendErrorStateCopyWithImpl<$Res>;
+  @useResult
+  $Res call({String message});
+}
+
+/// @nodoc
+class __$$FriendErrorStateCopyWithImpl<$Res>
+    extends _$FriendStateCopyWithImpl<$Res, _$FriendErrorState>
+    implements _$$FriendErrorStateCopyWith<$Res> {
+  __$$FriendErrorStateCopyWithImpl(
+      _$FriendErrorState _value, $Res Function(_$FriendErrorState) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? message = null,
+  }) {
+    return _then(_$FriendErrorState(
+      null == message
+          ? _value.message
+          : message // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$FriendErrorState implements FriendErrorState {
+  const _$FriendErrorState(this.message);
+
+  @override
+  final String message;
+
+  @override
+  String toString() {
+    return 'FriendState.error(message: $message)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$FriendErrorState &&
+            (identical(other.message, message) || other.message == message));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, message);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$FriendErrorStateCopyWith<_$FriendErrorState> get copyWith =>
+      __$$FriendErrorStateCopyWithImpl<_$FriendErrorState>(this, _$identity);
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() init,
+    required TResult Function() loading,
+    required TResult Function() loaded,
+    required TResult Function(String message) error,
+  }) {
+    return error(message);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? init,
+    TResult? Function()? loading,
+    TResult? Function()? loaded,
+    TResult? Function(String message)? error,
+  }) {
+    return error?.call(message);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? init,
+    TResult Function()? loading,
+    TResult Function()? loaded,
+    TResult Function(String message)? error,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error(message);
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(FriendInitState value) init,
+    required TResult Function(FriendLoadingState value) loading,
+    required TResult Function(FriendLoadedState value) loaded,
+    required TResult Function(FriendErrorState value) error,
+  }) {
+    return error(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(FriendInitState value)? init,
+    TResult? Function(FriendLoadingState value)? loading,
+    TResult? Function(FriendLoadedState value)? loaded,
+    TResult? Function(FriendErrorState value)? error,
+  }) {
+    return error?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(FriendInitState value)? init,
+    TResult Function(FriendLoadingState value)? loading,
+    TResult Function(FriendLoadedState value)? loaded,
+    TResult Function(FriendErrorState value)? error,
+    required TResult orElse(),
+  }) {
+    if (error != null) {
+      return error(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class FriendErrorState implements FriendState {
+  const factory FriendErrorState(final String message) = _$FriendErrorState;
+
+  String get message;
+  @JsonKey(ignore: true)
+  _$$FriendErrorStateCopyWith<_$FriendErrorState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
