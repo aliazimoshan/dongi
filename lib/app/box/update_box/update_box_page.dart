1a84f3e22e3a631bbd6bd10d40df9cfbc6602580
Aliazimoshan <a.azimoshan@gmail.com>
2023-06-06 17:43:57 +0330

    add listener to update box page

--- lib/app/box/update_box/update_box_page.dart
+++ lib/app/box/update_box/update_box_page.dart
@@ -3,9 +3,12 @@ import 'dart:io';
 import 'package:dongi/models/box_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../constants/color_config.dart';
+import '../../../core/utils.dart';
 import '../../../widgets/appbar/appbar.dart';
+import '../controller/box_controller.dart';
 import 'update_box_widget.dart';
 
 class UpdateBoxPage extends HookConsumerWidget {
@@ -19,6 +22,19 @@ class UpdateBoxPage extends HookConsumerWidget {
     final boxDescription = useTextEditingController(text: boxModel.description);
     final oldBoxImage = useState<String?>(boxModel.image);
     final newBoxImage = useState<File?>(null);
+
+    /// by using listen we are not gonna rebuild our app
+    ref.listen<BoxState>(
+      boxNotifierProvider,
+      (previous, next) {
+        next.whenOrNull(
+            loaded: () {
+              showSnackBar(context, "Successfully Updated!!");
+              context.pop();
+            },
+            error: (message) => showSnackBar(context, message));
+      },
+    );
 
     return Scaffold(
       backgroundColor: ColorConfig.white,
