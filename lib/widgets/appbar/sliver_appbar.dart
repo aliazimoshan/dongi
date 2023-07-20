6b09c923c587a224d9241b1aa0a56dd75c498a00
Aliazimoshan <a.azimoshan@gmail.com>
2023-07-21 00:00:01 +0330

    finish SliverAppBar and it color animation

--- lib/widgets/appbar/sliver_appbar.dart
+++ lib/widgets/appbar/sliver_appbar.dart
@@ -1,118 +1,112 @@
+import 'dart:ui';
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 import '../../constants/color_config.dart';
 
-class SliverAppBarWidget extends NestedScrollView {
+class SliverAppBarWidget extends HookWidget {
   final Widget appbarTitle;
   final Widget child;
   final double? height;
-  final double? collapsedHeight;
+  //final double? collapsedHeight;
   final String? image;
-  SliverAppBarWidget({
+  const SliverAppBarWidget({
+    super.key,
     required this.appbarTitle,
     required this.child,
-    this.height,
-    this.collapsedHeight,
+    this.height = 150,
+    //this.collapsedHeight,
     this.image,
-    super.key,
-  }) : super(
-          headerSliverBuilder: headerSliverBuilder(
-            appbarTitle: appbarTitle,
-            height: height,
-            collapsedHeight: collapsedHeight,
-            image: image,
-          ),
-          body: sliverAppBarBody(child: child),
-        );
-}
+  });
 
-/// * ----- headerSliverBuilder
-NestedScrollViewHeaderSliversBuilder headerSliverBuilder({
-  required Widget appbarTitle,
-  double? height,
-  double? collapsedHeight,
-  String? image,
-}) {
-  return (BuildContext context, bool innerBoxIsScrolled) {
-    return [
-      sliverAppBarTitle(
-        context: context,
-        appbarTitle: appbarTitle,
-        height: height,
-        collapsedHeight: collapsedHeight,
-        image: image,
-      ),
-    ];
-  };
-}
+  @override
+  Widget build(BuildContext context) {
+    final scrollController = useScrollController();
 
-/// * ----- sliver app bar title
-SliverAppBar sliverAppBarTitle({
-  required BuildContext context,
-  required Widget appbarTitle,
-  double? height,
-  double? collapsedHeight,
-  String? image,
-}) {
-  return SliverAppBar(
-    expandedHeight: height ?? 150,
-    centerTitle: false,
-    pinned: false,
-    floating: true,
-    collapsedHeight: collapsedHeight,
-    elevation: 0,
-    flexibleSpace: Stack(
-      children: [
-        image != null
-            ? Positioned.fill(
-                child: ColorFiltered(
-                  colorFilter: const ColorFilter.mode(
-                    Colors.black54,
-                    BlendMode.darken,
-                  ),
-                  child: Image.network(
-                    image,
-                    fit: BoxFit.cover,
+    // useEffect to dispose the ScrollController when the widget is unmounted
+    //useEffect(() {
+    //  return () {
+    //    scrollController.dispose();
+    //  };
+    //}, [scrollController]);
+
+    /// * ----- headerSliverBuilder
+    SliverAppBar headerSliver() {
+      return SliverAppBar(
+        expandedHeight: height,
+        centerTitle: false,
+        pinned: true,
+        floating: true,
+        snap: true,
+        elevation: 0,
+        toolbarHeight: 80,
+        flexibleSpace: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+          // Calculate the padding based on the current height of the FlexibleSpaceBar
+          const double expandedPadding = 30;
+          const double collapsedPadding = 70;
+          final double currentPadding = math.max(
+            0,
+            lerpDouble(
+              collapsedPadding,
+              expandedPadding,
+              (constraints.maxHeight - kToolbarHeight) / (200 - kToolbarHeight),
+            )!,
+          );
+
+          // Calculate opacity based on the scroll offset
+          final opacity = (scrollController.offset / (height! - kToolbarHeight))
+              .clamp(0.2, 0.6);
+
+          return Stack(
+            children: [
+              image != null
+                  ? Positioned.fill(
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          Colors.black54.withOpacity(opacity),
+                          BlendMode.colorBurn,
+                        ),
+                        child: Image.network(
+                          image!,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
+                  : const SizedBox(),
+              FlexibleSpaceBar(
+                centerTitle: false,
+                titlePadding: EdgeInsets.fromLTRB(currentPadding, 0, 0, 10),
+                title: appbarTitle,
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  height: 16,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: ColorConfig.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16),
+                    ),
                   ),
                 ),
-              )
-            : const SizedBox(),
-        FlexibleSpaceBar(
-          centerTitle: false,
-          titlePadding: const EdgeInsets.symmetric(horizontal: 16),
-          title: appbarTitle,
-        ),
-        Positioned(
-          bottom: 0,
-          child: Container(
-            height: 16,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: ColorConfig.white,
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(16),
-                topRight: Radius.circular(16),
               ),
-            ),
-          ),
-        )
-      ],
-    ),
-  );
-}
+            ],
+          );
+        }),
+      );
+    }
 
-/// * ----- body
-Widget sliverAppBarBody({
-  required Widget child,
-}) {
-  return Container(
-    decoration: BoxDecoration(
-      color: ColorConfig.white,
-      //borderRadius: const BorderRadius.only(
-      //  topLeft: Radius.circular(16),
-      //  topRight: Radius.circular(16),
-      //),
-    ),
-    child: child,
-  );
+    return NestedScrollView(
+      controller: scrollController,
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        headerSliver(),
+      ],
+      body: child,
+    );
+  }
 }
