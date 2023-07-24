83b9301aaeb106052d96d203ab8823dd0a5b4147
Aliazimoshan <a.azimoshan@gmail.com>
2023-07-24 09:41:59 +0330

    change the structure of expense page

--- lib/app/expense/expense_detail/expense_detail_page.dart
+++ lib/app/expense/expense_detail/expense_detail_page.dart
@@ -4,22 +4,23 @@ import '../../../constants/color_config.dart';
 import '../../../widgets/appbar/sliver_appbar.dart';
 import 'expense_detail_widget.dart';
 
-class ExpenseDetailPage extends ConsumerWidget with ExpenseDetailWidget {
+class ExpenseDetailPage extends ConsumerWidget {
   const ExpenseDetailPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      backgroundColor: ColorConfig.primarySwatch,
-      appBar: AppBar(title: const Text("Super Market"), elevation: 0),
+      backgroundColor: ColorConfig.white,
+      //appBar: AppBar(title: const Text("Super Market"), elevation: 0),
       body: SliverAppBarWidget(
-        appbarTitle: userInfoRow(),
+        appbarTitle: const UserInfoExpenseDetail(),
         child: ListView(
+          padding: EdgeInsets.zero,
           shrinkWrap: true,
           physics: const NeverScrollableScrollPhysics(),
-          children: [
-            expenseInfo(),
-            memberList(),
+          children: const [
+            InfoExpenseDetail(),
+            MemberListExpenseDetail(),
           ],
         ),
       ),
--- lib/app/expense/expense_detail/expense_detail_widget.dart
+++ lib/app/expense/expense_detail/expense_detail_widget.dart
@@ -2,19 +2,22 @@ import 'package:dongi/widgets/card/grey_card.dart';
 import 'package:dongi/widgets/list_tile/list_tile.dart';
 import 'package:dongi/widgets/list_tile/list_tile_card.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../constants/color_config.dart';
 import '../../../constants/font_config.dart';
 import '../../../widgets/friends/friend.dart';
 
-class ExpenseDetailWidget {
-  /// * ----- total expense
-  userInfoRow() {
+class UserInfoExpenseDetail extends ConsumerWidget {
+  const UserInfoExpenseDetail({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
         ListTileWidget(
-          contentPadding: EdgeInsets.zero,
+          contentPadding: const EdgeInsets.only(right: 10),
           headerString: "Created By",
           titleString: "Lham",
           titleStringStyle: FontConfig.body2().copyWith(
@@ -28,13 +31,22 @@ class ExpenseDetailWidget {
             height: 40,
             width: 40,
           ),
-          trailing: Icon(Icons.info_outline, color: ColorConfig.secondary),
+          trailing: Icon(
+            Icons.info_outline,
+            color: ColorConfig.secondary,
+          ),
         ),
       ],
     );
   }
+}
 
-  boxReviewBody({required List<Widget> children}) {
+class BoxReviewExpenseDetail extends ConsumerWidget {
+  final List<Widget> children;
+  const BoxReviewExpenseDetail(this.children, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Expanded(
       flex: 3,
       child: Container(
@@ -53,51 +65,55 @@ class ExpenseDetailWidget {
       ),
     );
   }
+}
 
-  /// * ----- Expense info
-  expenseInfo() {
-    infoCard(String title, String content) {
-      return Expanded(
-        child: SizedBox(
-          height: 90,
-          child: GreyCardWidget(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Container(
-                      width: 20,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: ColorConfig.primarySwatch,
-                        borderRadius: BorderRadius.circular(5),
-                      ),
+class InfoExpenseDetail extends ConsumerWidget {
+  const InfoExpenseDetail({super.key});
+
+  infoCard(String title, String content) {
+    return Expanded(
+      child: SizedBox(
+        height: 90,
+        child: GreyCardWidget(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: ColorConfig.primarySwatch,
+                      borderRadius: BorderRadius.circular(5),
                     ),
-                    //const Spacer(),
-                    //Icon(
-                    //  Icons.more_vert,
-                    //  color: ColorConfig.primarySwatch,
-                    //),
-                  ],
-                ),
-                const Spacer(),
-                Text(
-                  title,
-                  style: FontConfig.overline(),
-                ),
-                const SizedBox(height: 5),
-                Text(
-                  content,
-                  style: FontConfig.body2(),
-                ),
-              ],
-            ),
+                  ),
+                  //const Spacer(),
+                  //Icon(
+                  //  Icons.more_vert,
+                  //  color: ColorConfig.primarySwatch,
+                  //),
+                ],
+              ),
+              const Spacer(),
+              Text(
+                title,
+                style: FontConfig.overline(),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                content,
+                style: FontConfig.body2(),
+              ),
+            ],
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
 
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
       child: Row(
@@ -111,12 +127,17 @@ class ExpenseDetailWidget {
       ),
     );
   }
+}
 
-  /// * ----- expenses list
-  memberList() {
+class MemberListExpenseDetail extends ConsumerWidget {
+  const MemberListExpenseDetail({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(16, 25, 16, 25),
       child: ListView.builder(
+        padding: EdgeInsets.zero,
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
         itemCount: 5,
