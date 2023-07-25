7adc7e67c78346549b229a47101dc7d0f5e4ca41
Aliazimoshan <a.azimoshan@gmail.com>
2023-07-25 17:11:32 +0330

    finish expense page

--- lib/app/expense/expense_detail/expense_detail_page.dart
+++ lib/app/expense/expense_detail/expense_detail_page.dart
@@ -1,28 +1,51 @@
+import 'package:dongi/app/expense/controller/expense_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../../../constants/color_config.dart';
+import '../../../core/utils.dart';
 import '../../../widgets/appbar/sliver_appbar.dart';
+import '../../../widgets/error/error.dart';
+import '../../../widgets/loading/loading.dart';
 import 'expense_detail_widget.dart';
 
 class ExpenseDetailPage extends ConsumerWidget {
-  const ExpenseDetailPage({super.key});
+  final String expenseId;
+  const ExpenseDetailPage({super.key, required this.expenseId});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final expenseDetail = ref.watch(getExpensesDetailProvider(expenseId));
+
+    //by using listen we are not gonna rebuild our app
+    ref.listen<ExpenseState>(
+      expenseNotifierProvider,
+      (previous, next) {
+        next.whenOrNull(
+          loaded: () => ref.refresh(getExpensesDetailProvider(expenseId)),
+          error: (message) => showSnackBar(context, message),
+        );
+      },
+    );
+
     return Scaffold(
       backgroundColor: ColorConfig.white,
-      //appBar: AppBar(title: const Text("Super Market"), elevation: 0),
-      body: SliverAppBarWidget(
-        appbarTitle: const UserInfoExpenseDetail(),
-        child: ListView(
-          padding: EdgeInsets.zero,
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          children: const [
-            InfoExpenseDetail(),
-            MemberListExpenseDetail(),
-          ],
-        ),
+      body: expenseDetail.when(
+        loading: () => const LoadingWidget(),
+        error: (error, stackTrace) => ErrorTextWidget(error),
+        data: (data) {
+          return SliverAppBarWidget(
+            appbarTitle: UserInfoExpenseDetail(creatorId: data.creatorId),
+            child: ListView(
+              padding: EdgeInsets.zero,
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                InfoExpenseDetail(expenseModel: data),
+                MemberListExpenseDetail(members: data.expenseUsers),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
--- lib/app/expense/expense_detail/expense_detail_widget.dart
+++ lib/app/expense/expense_detail/expense_detail_widget.dart
@@ -1,3 +1,5 @@
+import 'package:dongi/extensions/date_extension.dart';
+import 'package:dongi/models/expense_model.dart';
 import 'package:dongi/widgets/card/grey_card.dart';
 import 'package:dongi/widgets/list_tile/list_tile.dart';
 import 'package:dongi/widgets/list_tile/list_tile_card.dart';
@@ -6,69 +8,82 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../constants/color_config.dart';
 import '../../../constants/font_config.dart';
+import '../../../widgets/error/error.dart';
 import '../../../widgets/friends/friend.dart';
+import '../../../widgets/loading/loading.dart';
+import '../../auth/controller/auth_controller.dart';
 
 class UserInfoExpenseDetail extends ConsumerWidget {
-  const UserInfoExpenseDetail({super.key});
+  final String creatorId;
+  const UserInfoExpenseDetail({super.key, required this.creatorId});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        ListTileWidget(
-          contentPadding: const EdgeInsets.only(right: 10),
-          headerString: "Created By",
-          titleString: "Lham",
-          titleStringStyle: FontConfig.body2().copyWith(
-            color: ColorConfig.pureWhite,
-          ),
-          headerStringStyle: FontConfig.overline().copyWith(
-            color: ColorConfig.pureWhite.withOpacity(0.25),
-          ),
-          leading: FriendWidget(
-            backgroundColor: ColorConfig.grey,
-            height: 40,
-            width: 40,
-          ),
-          trailing: Icon(
-            Icons.info_outline,
-            color: ColorConfig.secondary,
-          ),
-        ),
-      ],
-    );
-  }
-}
+    final creator = ref.watch(getUserData(creatorId));
 
-class BoxReviewExpenseDetail extends ConsumerWidget {
-  final List<Widget> children;
-  const BoxReviewExpenseDetail(this.children, {super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Expanded(
-      flex: 3,
-      child: Container(
-        decoration: BoxDecoration(
-          color: ColorConfig.white,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(15),
-            topRight: Radius.circular(15),
-          ),
-        ),
-        child: ListView(
+    return creator.when(
+      loading: () => const LoadingWidget(),
+      error: (error, stackTrace) => ErrorTextWidget(error),
+      data: (data) {
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            Column(children: children),
+            ListTileWidget(
+              contentPadding: const EdgeInsets.fromLTRB(0, 2, 10, 2),
+              headerString: "Created By",
+              titleString: data.userName,
+              titleStringStyle: FontConfig.body2().copyWith(
+                color: ColorConfig.pureWhite,
+              ),
+              headerStringStyle: FontConfig.overline().copyWith(
+                color: ColorConfig.pureWhite.withOpacity(0.25),
+              ),
+              leading: FriendWidget(
+                backgroundColor: ColorConfig.grey,
+                height: 40,
+                width: 40,
+              ),
+              trailing: Icon(
+                Icons.info_outline,
+                color: ColorConfig.secondary,
+              ),
+            ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
+
+//class BoxReviewExpenseDetail extends ConsumerWidget {
+//  final List<Widget> children;
+//  const BoxReviewExpenseDetail(this.children, {super.key});
+//  @override
+//  Widget build(BuildContext context, WidgetRef ref) {
+//    return Expanded(
+//      flex: 3,
+//      child: Container(
+//        decoration: BoxDecoration(
+//          color: ColorConfig.white,
+//          borderRadius: const BorderRadius.only(
+//            topLeft: Radius.circular(15),
+//            topRight: Radius.circular(15),
+//          ),
+//        ),
+//        child: ListView(
+//          children: [
+//            Column(children: children),
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
 
 class InfoExpenseDetail extends ConsumerWidget {
-  const InfoExpenseDetail({super.key});
+  final ExpenseModel expenseModel;
+
+  const InfoExpenseDetail({super.key, required this.expenseModel});
 
   infoCard(String title, String content) {
     return Expanded(
@@ -115,14 +130,14 @@ class InfoExpenseDetail extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+      padding: const EdgeInsets.fromLTRB(16, 0, 16, 0),
       child: Row(
         children: [
-          infoCard("Total Balance", "\$200"),
+          infoCard("Cost", expenseModel.cost.toString()),
           const SizedBox(width: 10),
-          infoCard("Date", "Nov 20, 2022"),
+          infoCard("Date", expenseModel.createdAt!.toHumanReadableFormat()),
           const SizedBox(width: 10),
-          infoCard("Split By", "4"),
+          infoCard("Split By", expenseModel.expenseUsers.length.toString()),
         ],
       ),
     );
@@ -130,38 +145,47 @@ class InfoExpenseDetail extends ConsumerWidget {
 }
 
 class MemberListExpenseDetail extends ConsumerWidget {
-  const MemberListExpenseDetail({super.key});
+  final List<String> members;
+  const MemberListExpenseDetail({super.key, required this.members});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 25, 16, 25),
-      child: ListView.builder(
-        padding: EdgeInsets.zero,
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: 5,
-        itemBuilder: (context, i) {
-          return Padding(
-            padding: const EdgeInsets.only(bottom: 10),
-            child: ListTileCard(
-              visualDensity: const VisualDensity(vertical: 3),
-              titleString: 'Member name',
-              trailing: const Text("\$53"),
-              leading: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  color: ColorConfig.primarySwatch,
+    final expenseMember = ref.watch(getUsersListData(members));
+
+    return expenseMember.when(
+      loading: () => const LoadingWidget(),
+      error: (error, stackTrace) => ErrorTextWidget(error),
+      data: (data) {
+        return Padding(
+          padding: const EdgeInsets.fromLTRB(16, 25, 16, 25),
+          child: ListView.builder(
+            padding: EdgeInsets.zero,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 10),
+                child: ListTileCard(
+                  visualDensity: const VisualDensity(vertical: 3),
+                  titleString: data[index].userName,
+                  trailing: const Text("\$53"),
+                  leading: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: ColorConfig.primarySwatch,
+                    ),
+                  ),
+                  //subtitleString: "subtitle",
+                  //headerString: "header",
                 ),
-              ),
-              //subtitleString: "subtitle",
-              //headerString: "header",
-            ),
-          );
-        },
-      ),
+              );
+            },
+          ),
+        );
+      },
     );
   }
 }
