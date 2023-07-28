d8c048005ce4dfb5bcd3e0614b3e211507dabfa3
Aliazimoshan <a.azimoshan@gmail.com>
2023-07-28 19:27:33 +0330

    add cache image network fro sliver

--- lib/widgets/appbar/sliver_appbar.dart
+++ lib/widgets/appbar/sliver_appbar.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'dart:math' as math;
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
@@ -69,8 +70,8 @@ class SliverAppBarWidget extends HookWidget {
                           Colors.black54.withOpacity(opacity),
                           BlendMode.colorBurn,
                         ),
-                        child: Image.network(
-                          image!,
+                        child: CachedNetworkImage(
+                          imageUrl: image!,
                           fit: BoxFit.cover,
                         ),
                       ),
